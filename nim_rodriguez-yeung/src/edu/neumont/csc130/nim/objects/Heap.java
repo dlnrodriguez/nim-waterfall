9d7f285d4d248f0c91e51354a26ff37c18062a63
DLN <drodriguez@Dylons-MacBook-Pro.local>
2017-10-06 09:10:40 -0600

    .gitignore added
    Ignores *.xml changes.

--- nim_rodriguez-yeung/src/edu/neumont/csc130/nim/objects/Heap.java
+++ nim_rodriguez-yeung/src/edu/neumont/csc130/nim/objects/Heap.java
@@ -1,4 +1,15 @@
 package edu.neumont.csc130.nim.objects;
 
+import java.util.List;
+
 public class Heap {
+    private List<Block> blocks;
+
+    public boolean isEmpty() {
+        return blocks.isEmpty();
+    }
+
+    public boolean removeBlocks(int nofBlocks) {
+        return false;
+    }
 }
